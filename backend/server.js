256d671b13d1ec149b45a8c0c2bdf4e74cc96f30
abhishekptiwari <abhishektiwariaman@gmail.com>
2021-09-15 22:33:37 +0530

    controllers and routes-1

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 //Setting up Config File
 dotenv.config({path:'backend/config/config.env'});
 
+
+
 app.listen(process.env.PORT,()=>{
-    console.log(`Server Running on Port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+    console.log(`Server Running on Port ${process.env.PORT} in ${process.env.NODE_ENV}`);
 })
