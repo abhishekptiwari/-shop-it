827fad33ce8ea6895ad84cc3ed4dc1d8941dc9a9
abhishekptiwari <abhishektiwariaman@gmail.com>
2021-09-16 15:11:10 +0530

    config set

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database')
 
 //Setting up Config File
 dotenv.config({path:'backend/config/config.env'});
 
+//Connecting to database
+connectDatabase();
 
 
 app.listen(process.env.PORT,()=>{
