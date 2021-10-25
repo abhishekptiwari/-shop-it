b4854716dc9ce6c6ea209bcf550e209d75f30ddc
abhishekptiwari <abhishektiwariaman@gmail.com>
2021-10-25 21:02:37 +0530

    query String

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -6,7 +6,13 @@ class APIFeatures {
 
     search() {
         const keyword = this.queryStr.keyword ? {
-
+          name:{
+              $regex:this.queryStr.keyword,
+              $options:'i'
+          }
         }:{}
+
+        this.query = this.query.find({...keyword});
+        return this ; 
     }
 }
